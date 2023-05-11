eab4e7b5cff9ac18f697ba332d852c6f7b865cf5
IvinoDev <kayantao@protonmail.com>
2023-05-11 10:53:37 +0000

    initial commit

---
+++ src/main/java/ml/ivinodev/flywayexemple/FlyWayExempleApplication.java
@@ -0,0 +1,13 @@
+package ml.ivinodev.flywayexemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyWayExempleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyWayExempleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ml/ivinodev/flywayexemple/FlyWayExempleApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.ivinodev.flywayexemple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyWayExempleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
